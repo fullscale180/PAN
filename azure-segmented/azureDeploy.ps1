a4270ac7b30628e18128949a0f2a204067b03b31
Dennis Angeline <dangeline@live.com>
2017-04-27 15:31:26 -0700

    start segmented deployment

---
+++ azure-segmented/azureDeploy.ps1
@@ -0,0 +1,46 @@
+﻿#-----------------------------------------------------
+# Import utilities module
+#-----------------------------------------------------
+Import-module AzureRM.Compute
+Import-module AzureRM.Storage
+Import-module AzureRM.Network
+#Import-module AzureRM.ApiManagement
+#Import-module AzureRM.Automation
+#Import-module AzureRM.Backup
+#Import-module AzureRM.KeyVault
+#Import-module AzureRM.WebSites
+#Import-module AzureRM.Resources
+
+#-----------------------------------------------------
+# Authenticate
+#-----------------------------------------------------
+Login-AzureRmAccount
+
+#-----------------------------------------------------
+# Select the Subscription by subscriptionId or by Tenant Id
+#-----------------------------------------------------
+Get-AzureRmSubscription -SubscriptionName "Visual Studio Enterprise" | Select-AzureRMSubscription 
+
+#-----------------------------------------------------
+# Create a new deployment
+#-----------------------------------------------------
+cd 'D:\All Projects\FS180 Clients\PAN\azure-segmented'
+
+$deployName = "AzureDeploy"
+$location = "westus2"
+
+#-----------------------------------------------------
+# Deploy outbound firewall
+#-----------------------------------------------------
+$rgName = "PAN1" 
+$rg = New-AzureRMResourceGroup -name $rgName -location $location
+New-AzureRMResourceGroupDeployment -ResourceGroupName $rgName -Name $deployName -TemplateFile .\azureDeployInfra.json -TemplateParameterFile .\azureDeployInfra.parameters.json 
+
+#-----------------------------------------------------
+# Deploy application firewall and backend
+#-----------------------------------------------------
+$rgName = "PAN1" 
+$rg = New-AzureRMResourceGroup -name $rgName -location $location
+New-AzureRMResourceGroupDeployment -ResourceGroupName $rgName -Name $deployName -TemplateFile .\azureDeployApp.json -TemplateParameterFile .\azureDeployApp.parameters.json 
+
+
